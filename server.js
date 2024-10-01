608e8b575af76ac712da9f8be41745b1fe35fd2f
tratchapong <tratchapong@gmail.com>
2024-10-01 11:38:44 +0700

    document and basic middlewares

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const notFound = require('./middlewares/not-found')
+const errorMiddleware = require('./middlewares/error-middleware')
+
+
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 
+app.use('/auth', (req,res)=>{ 
+	// console.log(first)
+	res.json('Auth')
+})
+app.use('/post', ()=>{})
+app.use('/comment', ()=>{})
+app.use('/like', ()=>{})
+
+app.use(notFound)
+app.use(errorMiddleware)
 
 const port = process.env.PORT || 8800
 app.listen(port, ()=>console.log('Server on',port))
