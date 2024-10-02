71345907ab3bf2bacc6552e203dbd5896a18aa3d
tratchapong <tratchapong@gmail.com>
2024-10-02 10:11:37 +0700

    make createError in utils

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,18 +1,21 @@
 const prisma = require('../models')
+const createError = require('../utils/createError')
 const tryCatch = require('../utils/tryCatch')
 
 module.exports.register = tryCatch(async (req,res) => {
 	const { identity,firstName, lastName, password, confirmPassword } = req.body
 	// validation
 	if( !(identity.trim() && firstName.trim() && lastName && password && confirmPassword) ) {
-		throw(new Error("Please fill all data"))
+		throw(createError(400, "Please fill all data"))
 	}
 
 	if(password !== confirmPassword) {
-		throw(new Error("check confirm Password"))
+		throw(createError(400,"check confirm Password"))
 	}
 	res.json('Register Controller...')
 })
+
+
 
 module.exports.login = tryCatch(async (req, res) => {
 	res.json('Login Controller')
