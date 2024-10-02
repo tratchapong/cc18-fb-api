da253ebe11ec58bb6bcbbdb48c346ba5a5af35e0
tratchapong <tratchapong@gmail.com>
2024-10-02 11:09:27 +0700

    Register done

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const prisma = require('../models')
+const bcrypt = require('bcryptjs')
 const createError = require('../utils/createError')
 const tryCatch = require('../utils/tryCatch')
 
@@ -12,8 +13,44 @@ module.exports.register = tryCatch(async (req,res) => {
 	if(password !== confirmPassword) {
 		createError(400,"check confirm Password")
 	}
-	res.json('Register Controller...')
+	// check identity is mobile or email
+	let identityKey = ''
+	if(/^[0-9]{10,15}$/.test(identity)) {
+		identityKey = 'mobile'
+	}
+	if(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(identity)) {
+		identityKey = 'email'
+	}
+	if(!identityKey) {
+		createError(400, 'only email or phone number')
+	}
+
+	// check if already email / mobile in User data
+
+	const findIdentity = await prisma.user.findUnique({
+		where : { [identityKey] : identity}
+	})
+
+	if(findIdentity) {
+		createError(409,`Already have this ${identityKey}`)
+	}
+
+	// create user in db
+	const hashedPassword = await bcrypt.hash(password,10)
+
+	const newUser = {
+		[identityKey] : identity,
+		password : hashedPassword,
+		firstName,
+		lastName
+	}
+
+	const result = await prisma.user.create({data: newUser})
+
+	res.json({msg: 'Register successful', result})
 })
+
+
 
 
 
