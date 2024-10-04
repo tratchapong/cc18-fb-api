81460ea393d60a64cdf3648425a78220f686fe6e
tratchapong <tratchapong@gmail.com>
2024-10-04 15:07:05 +0700

    createPost : no image

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,12 +1,17 @@
+const tryCatch = require("../utils/tryCatch")
+const prisma = require('../models')
 
 
 module.exports.getAllPosts = (req, res) => {
 	res.json('getAllPost')
 }
 
-module.exports.createPost = (req, res) => {
-	res.json('createPost')
-}
+module.exports.createPost = tryCatch( async (req, res) => {
+	const {message } = req.body
+	const data = { message, userId : req.user.id }
+	const rs = await prisma.post.create({ data })
+	res.json(rs)
+})
 
 module.exports.editPost = (req, res) => {
 	const { id } = req.params
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const notFound = require('./middlewares/not-found')
 const errorMiddleware = require('./middlewares/error-middleware')
 const authRoute = require('./routes/auth-route')
 const postRoute = require('./routes/post-route')
+const authenticate = require('./middlewares/authenticate')
 
 
 const app = express()
@@ -13,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRoute)
-app.use('/post', postRoute)
+app.use('/post', authenticate, postRoute)
 app.use('/comment', ()=>{})
 app.use('/like', ()=>{})
 
